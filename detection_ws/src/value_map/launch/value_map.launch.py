d01d1b30efea1a337b7fbb7754d8b60871f3885e
KevinEppacher <kevin-eppacher@hotmail.de>
2025-06-06 14:07:23 +0200

    detection_ws:value_map: Added launch and rviz file

---
+++ detection_ws/src/value_map/launch/value_map.launch.py
@@ -0,0 +1,33 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from launch_ros.substitutions import FindPackageShare
+import os
+
+def generate_launch_description():
+    package_name = 'value_map'
+
+    # Find config and rviz path
+    pkg_share = FindPackageShare(package_name).find(package_name)
+    rviz_config = os.path.join(pkg_share, 'rviz', 'rviz.rviz')
+    # param_file = os.path.join(pkg_share, 'config', 'value_map.yml')
+
+    value_map_node = Node(
+        package=package_name,
+        executable='value_map_node',
+        name="value_map_node",
+        output='screen',
+        emulate_tty=True,
+        # parameters=[param_file],
+        # arguments=['--ros-args', '--log-level', 'debug']
+    )
+
+    rviz_node = ExecuteProcess(
+        cmd=['rviz2', '-d', rviz_config],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        value_map_node,
+        rviz_node
+    ])
