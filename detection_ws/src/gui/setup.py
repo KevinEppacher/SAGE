8526e265842ed168593d4fe24c190dba5fde11e0
KevinEppacher <kevin-eppacher@hotmail.de>
2025-07-19 23:17:52 +0200

    Add new static and template directories; update setup.py for package data inclusion; modify cloud cluster to use PointXYZI; update semantic pointcloud topic; remove unused frames image; update openfusion_ros submodule

--- detection_ws/src/gui/setup.py
+++ detection_ws/src/gui/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'gui'
 
@@ -10,7 +12,14 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Add templates and static files
+        (os.path.join('share', package_name, 'templates'), glob('gui/templates/*.html')),
+        (os.path.join('share', package_name, 'static'), glob('gui/static/*')),
     ],
+    include_package_data=True,
+    package_data={
+        'gui': ['templates/*.html', 'static/*']
+    },
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ubuntu',
