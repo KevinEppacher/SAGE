a0cee9f2a6b84b803c494145438596c61bddce9e
KevinEppacher <kevin-eppacher@hotmail.de>
2025-07-28 00:46:15 +0200

    Enhance Graph Node Fusion with additional subscriptions, publishers, and timer functionality; update package dependencies for geometry_msgs

--- explorer_ws/src/graph_node_fusion/graph_node_fusion/graph_node_fusion.py
+++ explorer_ws/src/graph_node_fusion/graph_node_fusion/graph_node_fusion.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from graph_node_msgs.msg import GraphNode, GraphNodeArray
+from geometry_msgs.msg import PoseStamped
 
 class GraphNodeFusion(Node):
     def __init__(self):
@@ -8,14 +9,37 @@ class GraphNodeFusion(Node):
         self.get_logger().info('Graph Node Fusion has been initialized')
 
         # Subscribers
-        self.graph_node_subscriber = self.create_subscription(
+        self.exploration_graph_node_sub = self.create_subscription(
             msg_type=GraphNodeArray,
             topic='/semantic_frontier_exploration/graph_nodes',
-            callback=self.graph_node_callback,
+            callback=self.exploration_graph_node_callback,
             qos_profile=10
         )
 
-    def graph_node_callback(self, msg):
+        self.exploitation_graph_node_sub = self.create_subscription(
+            msg_type=GraphNodeArray,
+            topic='/semantic_frontier_exploration/graph_nodes',
+            callback=self.exploitation_graph_node_callback,
+            qos_profile=10
+        )
+
+        # Publishers
+        self.goal_pose_publisher = self.create_publisher(
+            msg_type=PoseStamped,
+            topic='/semantic_frontier_exploration/goal_pose',
+            qos_profile=10
+        )
+
+        # Timers
+        self.timer = self.create_timer(1.0, self.timer_callback)
+
+        # Member Variables
+        self.current_goal = None
+        self.fused_graph_nodes = []
+        self.exploration_graph_nodes = []
+        self.exploitation_graph_nodes = []
+
+    def exploration_graph_node_callback(self, msg):
         self.get_logger().info(f'Received {len(msg.nodes)} graph nodes')
 
         # Sorting the nodes based on score
@@ -24,3 +48,16 @@ class GraphNodeFusion(Node):
         # Log the sorted nodes
         for node in sorted_nodes:
             self.get_logger().info(f'Node ID: {node.id}, Score: {node.score}')
+
+    def exploitation_graph_node_callback(self, msg):
+        self.get_logger().info(f'Received {len(msg.nodes)} graph nodes')
+
+        # Sorting the nodes based on score
+        sorted_nodes = sorted(msg.nodes, key=lambda node: node.score, reverse=True)
+
+        # Log the sorted nodes
+        for node in sorted_nodes:
+            self.get_logger().info(f'Node ID: {node.id}, Score: {node.score}')
+
+    def timer_callback(self):
+        self.get_logger().info('Timer triggered')
