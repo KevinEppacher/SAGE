c9e0d7fbb73f1820862fe662d1d43d9af09f114c
KevinEppacher <kevin-eppacher@hotmail.de>
2025-10-24 13:01:24 +0200

    Enhance GraphNodeArray and graph_node_fusion configurations; add detection topic, adjust approach radius, and update RViz settings for improved visualization and functionality

--- explorer_ws/src/semantic_frontier_exploration/semantic_frontier_exploration/launch/semantic_frontier_exploration.launch.py
+++ explorer_ws/src/semantic_frontier_exploration/semantic_frontier_exploration/launch/semantic_frontier_exploration.launch.py
@@ -1,35 +1,56 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import ExecuteProcess
-from launch_ros.substitutions import FindPackageShare
-from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.actions import  ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import PushRosNamespace
+from launch.substitutions import LaunchConfiguration
 import os
 
 def generate_launch_description():
-    package_name = 'semantic_frontier_exploration'
-    pkg_share = FindPackageShare(package_name).find(package_name)
 
-    rviz_config = os.path.join(pkg_share, 'rviz', 'rviz.rviz')
-    param_file = os.path.join(pkg_share, 'config', 'sem_frontiers.yml')
+    sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time', default_value='true',
+        description='Flag to enable use_sim_time'
+    )
+
+    # Get the launch configuration for use_sim_time
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    rviz_config = os.path.join(
+        get_package_share_directory('semantic_frontier_exploration'),
+        'rviz',
+        'rviz.rviz'
+    )
+
+
+    explorer_config = os.path.join(
+        get_package_share_directory('semantic_frontier_exploration'),
+        'config',
+        'sem_frontiers.yaml'
+    )
 
     semantic_frontiers_node = Node(
-        package=package_name,
+        package='semantic_frontier_exploration',
         executable='semantic_frontier_exploration_node',
         name="semantic_frontier_exploration_node",
+        namespace='exploration_graph_nodes',
         output='screen',
         emulate_tty=True,
-        parameters=[param_file]
+        parameters=[
+            explorer_config,
+            {'use_sim_time': use_sim_time}
+        ],            
     )
+
 
     rviz_node = ExecuteProcess(
         cmd=['rviz2', '-d', rviz_config],
         output='screen'
     )
 
-    return LaunchDescription([
-        PushRosNamespace('semantic_frontier_exploration'),
-        semantic_frontiers_node,
-        rviz_node
-    ])
+    ld = LaunchDescription()
+    ld.add_action(sim_time_arg)
+    ld.add_action(semantic_frontiers_node)
+    ld.add_action(rviz_node)
+    return ld
