c3228066eb86242e5a87e560b19859892824c0ab
KevinEppacher <kevin-eppacher@hotmail.de>
2026-01-23 23:41:03 +0100

    Refactor code structure for improved readability and maintainability

--- detection_ws/src/gui/gui/gui.py
+++
@@ -1,9 +0,0 @@
-import rclpy
-from gui.webgui import WebGUI
-from gui.ros2backend import ROS2Backend
-
-def main():
-    rclpy.init()
-    ros_backend = ROS2Backend()
-    gui = WebGUI(ros_backend)
-    gui.run()
